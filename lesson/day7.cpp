897bc22c01f3de1fca0cc6474a60c3092c643db5
PANHARO <panharokeo097@gmail.com>
2026-09-16 23:31:57 +0700

    hw

--- lesson/day7.cpp
+++ lesson/day7.cpp
@@ -3,13 +3,41 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i < 6; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            cout << "*";
+    // first exercise
+    // for(int i=0; i<4; i++){
+    //     for(int j=0; j<4; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // second exercise
+    // for (int i = 1; i < 6; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // third exercise
+    // for(int i=0; i<5; i++){
+    //     for(int j =5; j>i; j--){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //fourth exercise
+    for(int i=0; i<5; i++){
+        for(int j=0; j<4-i; j++){
+            cout<<" ";
         }
-        cout << endl;
+        for(int k=0; k<=i; k++){
+                cout<<"*";
+        }
+        cout<<endl;
     }
     return 0;
 }   
